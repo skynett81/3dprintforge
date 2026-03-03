8391595b65cedc9bb2c97f6957eae19371b42627
SkyNett81 <skynett81@users.noreply.github.com>
2026-03-03 17:13:49 +0100

    Fix CSP blocking exchange rate API
    Add https://open.er-api.com to connect-src directive so currency
    exchange rates can be fetched for locale-based formatting.

--- server/index.js
+++ server/index.js
@@ -82,7 +82,7 @@ const SECURITY_HEADERS = {
     "script-src 'self' 'unsafe-inline'",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "img-src 'self' data: blob: https://*.bblmw.com https://public-cdn.bblmw.com",
-    "connect-src 'self' ws: wss:",
+    "connect-src 'self' ws: wss: https://open.er-api.com",
     "font-src 'self' https://fonts.gstatic.com",
     "object-src 'none'",
     "base-uri 'self'",
